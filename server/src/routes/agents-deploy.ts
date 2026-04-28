4017ed1f5dfb8fb44ecd3968700f5d7d4be27793
SATISH-JALAN <satishjalan9163@gmail.com>
2026-04-28 23:08:25 +0530

    migrate from Stellar to Solana services and update TypeScript version

--- server/src/routes/agents-deploy.ts
+++ server/src/routes/agents-deploy.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+﻿import { Router, Request, Response } from 'express';
 import * as StellarSdk from 'stellar-sdk';
 import crypto from 'node:crypto';
 import {
--- server/src/routes/faucet.ts
+++ server/src/routes/faucet.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+﻿import { Router, Request, Response } from 'express';
 import { Keypair, Networks, Asset, Memo, TransactionBuilder, Operation, Horizon } from 'stellar-sdk';
 import Ably from 'ably';
 
